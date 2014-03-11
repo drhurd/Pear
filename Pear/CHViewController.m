5861fe335f5525649a45db2a3b09ae294d45ae3d
birwin93 <birwin93@gmail.com>
2014-03-11 17:40:22 -0400

    added firebase code

--- Pear/CHViewController.m
+++ Pear/CHViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CHViewController.h"
+#import <Firebase/Firebase.h>
 
 @interface CHViewController ()
 
@@ -17,6 +18,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSString* url = @"https://pearsync.firebaseio.com/sound";
+    Firebase* dataRef = [[Firebase alloc] initWithUrl:url];
+    [dataRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        [self renderSignal:snapshot];
+    }];
+
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +33,32 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)play:(id)sender {
+    Firebase* f = [[Firebase alloc] initWithUrl:@"https://pearsync.firebaseio.com/sound"];
+    
+    // Write data to Firebase
+    NSArray *vals = @[@"red", @"green", @"blue", @"orange"];
+    int index = arc4random()%4;
+    NSLog(@"index %i", index);
+    
+    [f setValue:vals[index]];
+}
+
+- (void)renderSignal:(FDataSnapshot*)snapshot {
+    NSLog(@"snapshot: %@", snapshot);
+    if (snapshot.value) {
+        if ([snapshot.value isEqualToString:@"red"]) {
+            self.view.backgroundColor = [UIColor redColor];
+        } else if ([snapshot.value isEqualToString:@"blue"]) {
+            self.view.backgroundColor = [UIColor blueColor];
+        } else if ([snapshot.value isEqualToString:@"orange"]) {
+            self.view.backgroundColor = [UIColor orangeColor];
+        } else if ([snapshot.value isEqualToString:@"green"]) {
+            self.view.backgroundColor = [UIColor greenColor];
+        }
+    }
 }
 
 @end
